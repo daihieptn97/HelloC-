1fd457c1bd0f261b0271889cc784dfa834f7fab0
Trần Đại Hiệp <daihieptn97@gmail.com>
2021-07-08 22:29:13 +0700

    init

---
+++ hello.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+
+
+using namespace std;
+
+
+// comment 
+
+// ham 
+
+// bien
+
+//gia tri
+
+int main(){
+
+//	int a, b, tong;
+	
+//	cout <<"Nhap so a = ";
+//	cin >> a;
+	
+//	cout << endl;
+//	cout <<"Nhap so b = ";
+//	cin >> b;
+	
+	
+//	tong = a + b;
+	
+//	cout <<"tong= "<< tong << endl;
+	
+	int number1, number2, trang;
+	
+	cout << "hien so number1 ";
+	cin >> number1;
+	
+	cout << "hien so number2 ";
+	cin >> number2;
+	
+	trang = number1 *  number2;
+	
+	cout <<"tong ="<< trang << endl;
+	
+	return 0;
+}
