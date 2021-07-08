6a31e3edce1f344d3bfb0531f7c61d193b19a270
Trần Đại Hiệp <daihieptn97@gmail.com>
2021-07-08 23:31:14 +0700

    init

---
+++ giaiphuongtrinhbacnhat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+
+using namespace std;
+	
+int main() {
+	
+	// ax+b=0 
+	
+	float a, b, x;
+	
+	cout <<"a =";
+	cin>> a;
+	
+	cout <<"b =";
+	cin>> b;
+	 
+	x = -b/a;
+	
+	cout <<"x ="<< x <<endl;
+	return 0;
+}
---
+++ hello2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+
+
+using namespace std;
+
+
+
+int main(){
+
+
+ 	string a = "Hello World!";
+ 	
+ 	cout << a;
+
+	return 0;
+}
